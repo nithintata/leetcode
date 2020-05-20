a79d79fe8b9b5b26b00bf8928c6af07a1af652ee
Nithin Kumar <43900757+nithintata@users.noreply.github.com>
2020-05-20 20:44:30 +0530

    day 20

---
+++ may challenge/20.java
@@ -0,0 +1,29 @@
+// Kth Smallest Element in a BST
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !st.isEmpty()) {
+            while (cur != null) {
+                st.push(cur);
+                cur = cur.left;
+            }
+            cur = st.pop();
+            k--;
+            if (k == 0)
+                return cur.val;
+            cur = cur.right;
+        }
+        return -1;
+    }
+}
