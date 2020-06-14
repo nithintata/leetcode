3695db82578757b1a790e97c99aa94b2e869acf7
Nithin Kumar <43900757+nithintata@users.noreply.github.com>
2020-06-14 20:39:06 +0530

    day 12

---
+++ june challenge/12.java
@@ -0,0 +1,44 @@
+// Insert Delete GetRandom O(1)
+
+class RandomizedSet {
+
+    Set<Integer> set;
+    Random rand = new Random();
+    ArrayList<Integer> list;
+    /** Initialize your data structure here. */
+    public RandomizedSet() {
+        set = new HashSet<>();
+        list = new ArrayList<>();
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        if (set.contains(val))
+            return false;
+        set.add(val);
+        list.add(val);
+        return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        if (!set.contains(val))
+            return false;
+        set.remove(val);
+        list.remove(new Integer(val));
+        return true;
+    }
+    
+    /** Get a random element from the set. */
+    public int getRandom() {
+        return list.get(rand.nextInt(list.size()));
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
